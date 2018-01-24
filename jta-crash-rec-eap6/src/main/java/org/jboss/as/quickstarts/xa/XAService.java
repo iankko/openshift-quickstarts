9d72a9edf5698e0de88b62f59b967bec2e377877
Jan Lieskovsky <jlieskov@redhat.com>
2018-01-24 08:54:26 +0100

    Keep transaction in prepared state for longer time

--- jta-crash-rec-eap6/src/main/java/org/jboss/as/quickstarts/xa/XAService.java
+++ jta-crash-rec-eap6/src/main/java/org/jboss/as/quickstarts/xa/XAService.java
@@ -184,6 +184,9 @@ public class XAService {
 
             result.append(listPairs());
 
+            // Pause for 3 hours before commit
+            Thread.sleep(10800000);
+
             userTransaction.commit();
         } catch (Exception e) {
             result.append(e.getCause() != null ? e.getCause().getMessage() : e.getMessage());
